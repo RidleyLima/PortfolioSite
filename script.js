fdb3f69a958f43bdbd008698062c6e155c5b66ae
Ridley <ridleylmbr2015@gmail.com>
2026-09-02 21:00:20 -0300

    feat: Added Services Section.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+function escrevendo(){
 function toggleEscrita(elemento){
     const arrTexto = elemento.innerHTML.split('');
     elemento.innerHTML = '';
@@ -11,3 +12,13 @@ function toggleEscrita(elemento){
 
 const titulo = document.querySelector('.digitando');
 toggleEscrita(titulo);
+}
+
+escrevendo();
+
+const menuIcon = document.querySelector('.fa-bars');
+    menuIcon.addEventListener('click', () => {
+        menuIcon.classList.toggle('fa-xmark');
+        const nav = document.querySelector('nav');
+        nav.classList.toggle('active');
+    })
